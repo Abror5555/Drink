7e13ae9ca17aa4b2c3261ce10eb31da97b602c6f
Abror Qo'shshiyev <107792031+Abror5555@users.noreply.github.com>
2024-02-03 12:00:46 +0500

    Update serializers.py

--- drinks/serializers.py
+++ drinks/serializers.py
@@ -4,4 +4,4 @@ from drinks.models import Drink
 class DirnkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drink
-        fields = ['id', 'name', 'description', 'img']
+        fields = ['id', 'url', 'name', 'description', 'img']
